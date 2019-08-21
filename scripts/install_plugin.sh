457d954216c35f1d3dfb73428ae9dcf80c3c9238
l10xbin <40697655+l10xbin@users.noreply.github.com>
2019-08-21 11:28:37 +0800

    Update install_plugin.sh
    fix typo

--- scripts/install_plugin.sh
+++ scripts/install_plugin.sh
@@ -14,7 +14,7 @@ url=""
 if [ "$(uname)" = "Darwin" ]; then
     url="https://github.com/chartmuseum/helm-push/releases/download/v${version}/helm-push_${version}_darwin_amd64.tar.gz"
 elif [ "$(uname)" = "Linux" ] ; then
-    url="https://github.com//l10xbin/releases/download/v${version}/helm-push_${version}_linux_arm64.tar.gz"
+    url="https://github.com/l10xbin/releases/download/v${version}/helm-push_${version}_linux_arm64.tar.gz"
 else
     url="https://github.com/chartmuseum/helm-push/releases/download/v${version}/helm-push_${version}_windows_amd64.tar.gz"
 fi
